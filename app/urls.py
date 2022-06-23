f59652086da336100e7b0bdda2a98e63bf797ab8
kleitich <kevleitich@gmail.com>
2022-06-23 10:52:05 +0300

    registering url for registration

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 
 # create url patterns
 urlpatterns=[
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('signup', views.signup, name = 'signup'),
+    path('manager_signup', views.manager_signup_view.as_view(),name='manager_signup'),
+    path('employee_signup', views.employee_signup_view.as_view(),name='employee_signup'),
+    path('client_signup', views.client_signup_view.as_view(),name='client_signup'),
+    path('login',views.login_view,name="login_view"),
+    path('logout',views.logout,name='logout'),
+    
 ]
+
