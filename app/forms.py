271a2b68039712b47d5fd052917efc49bfe79f77
kleitich <kevleitich@gmail.com>
2022-06-23 11:02:02 +0300

    adding form for registration

---
+++ app/forms.py
@@ -0,0 +1,98 @@
+from django import forms
+from django.db import transaction
+from django.forms import ModelForm
+from .models import *
+from django.contrib.auth.forms import UserCreationForm 
+
+
+class manager_signup(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email_address = forms.EmailField(required=True)
+    phone_number = forms.CharField(required=True)
+    profile_pic = forms.ImageField()
+
+    class Meta (UserCreationForm.Meta):
+        model = User
+    @transaction.atomic
+    def save(self):
+            user = super().save(commit=False)
+            user.first_name = self.cleaned_data.get('first_name')
+            user.last_name = self.cleaned_data.get('last_name')
+            user.is_matron = True
+            user.save()
+            manager = Manager.objects.create(user=user)
+            manager.email_address = self.cleaned_data.get('email_address')
+            manager.phone_number = self.cleaned_data.get('phone_number')
+            manager.profile_pic = self.cleaned_data.get('profile_pic')
+
+            return manager
+        
+
+class employee_signup(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.EmailField()
+    registration_no = forms.CharField(required=True)
+    phone_number = forms.CharField(required=True)
+    profile_pic = forms.ImageField()
+    gender = forms.CharField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')        
+        user.is_student = True
+        user.save()
+        employee = Employee.objects.create(user=user)
+        employee.registration_no = self.cleaned_data.get('reg_no')
+        employee.email = self.cleaned_data.get('email')
+        employee.phone_number = self.cleaned_data.get('phone_number')
+        employee.profile_pic = self.cleaned_data.get('profile_pic')
+        employee.gender = self.cleaned_data.get('gender')
+
+        return employee
+
+class client_signup(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.EmailField()
+    registration_no = forms.CharField(required=True)
+    phone_number = forms.CharField(required=True)
+    profile_pic = forms.ImageField()
+    gender = forms.CharField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')        
+        user.is_student = True
+        user.save()
+        client = Client.objects.create(user=user)
+        client.registration_no = self.cleaned_data.get('reg_no')
+        client.email = self.cleaned_data.get('email')
+        client.phone_number = self.cleaned_data.get('phone_number')
+        client.profile_pic = self.cleaned_data.get('profile_pic')
+        client.gender = self.cleaned_data.get('gender')
+
+        return client
+
+class login_form(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class":"form-control"}
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class":"form-control"}
+        )
+    )
--- app/views.py
+++ app/views.py
@@ -1,5 +1,56 @@
 from django.shortcuts import render,redirect
-
+from app.forms import employee_signup, login_form, manager_signup
+from .models import User
+from django.views.generic import CreateView
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def signup(request):
+    return render(request,'register.html' )
+
+class matron_signup_view (CreateView):
+    model = User
+    form_class = manager_signup
+    template_name = 'sign_up.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs ['user_type'] = 'matron'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        return redirect('/login')
+
+class student_signup_view(CreateView):
+    model = User
+    form_class = employee_signup
+    template_name = 'sign_up.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs ['user_type'] = 'student'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        return redirect('/login')
+
+def login_view(request):
+  form = login_form(request.POST or None)
+  if request.method == 'POST':
+    if form.is_valid():
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password')
+      user = authenticate(username=username,password=password)
+      if user is not None and user.is_matron:
+        login(request, user)
+        return redirect('/')
+      elif user is not None and user.is_student:
+        login(request, user)
+        return redirect('/')
+  return render (request, 'login.html',{'form':form})
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect ('/')
